dd2d570890d105a8dd72e33fa9c714cc7dd68932
peachdew <chinfurui@gmail.com>
2024-06-22 12:11:51 +0800

    Portals WORK!!!

--- Objects/Portals/portal.gd
+++ Objects/Portals/portal.gd
@@ -1,13 +1,15 @@
 extends Area2D
 class_name Portal
 
-var stay_time := 1.5
+var stay_time := 3
 var curr_stay_time := 0.0
 var contains_player := false
 # Called when the node enters the scene tree for the first time.
 signal send_player_to
 
 @export var destination_scene_path := "res://first_area_name.tscn"
+
+@onready var portal_animated_sprite := $AnimatedSprite2D
 
 func _ready() -> void:
 	body_entered.connect(on_body_entered)
@@ -19,8 +21,11 @@ func _physics_process(delta: float) -> void:
 		contains_player = false
 		
 	if contains_player:
+		if portal_animated_sprite.animation == "idle":
+			portal_animated_sprite.play("going")
 		curr_stay_time += delta
 	else:
+		portal_animated_sprite.play("idle")
 		curr_stay_time = 0
 	
 func on_body_entered(body):
